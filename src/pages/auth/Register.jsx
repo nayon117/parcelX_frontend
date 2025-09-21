e14185b89f576c59933ee5d1aee57ac6edf57f29
nayon117 <hasibul.nayon1@gmail.com>
2025-09-21 15:00:42 +0600

    create authprovider and make registration functional

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 const Register = () => {
   const {
@@ -8,42 +9,73 @@ const Register = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const { createUser } = useAuth();
+
+  const onSubmit = (data) => {
+    createUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
   return (
     <div className="text-black mt-3">
-
-    {/* header */}
+      {/* header */}
       <h1 className="text-2xl font-bold">Create an Account</h1>
       <p className="mb-4">Register with parcelX</p>
-      <form onSubmit={handleSubmit(onSubmit)} >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset className="fieldset">
-
           {/* Name */}
           <label className="label">Name</label>
-          <input {...register("name", { required: true })} type="text" className="input bg-white " placeholder="Name" />
-          {errors.name && <span className="text-red-500">Name is required</span>}
+          <input
+            {...register("name", { required: true })}
+            type="text"
+            className="input bg-white "
+            placeholder="Name"
+          />
+          {errors.name && (
+            <span className="text-red-500">Name is required</span>
+          )}
 
-         {/* email */}
+          {/* email */}
           <label className="label">Email</label>
-          <input {...register("email", { required: true })} type="email" className="input bg-white " placeholder="Email" />
-          {errors.email && <span className="text-red-500">Email is required</span>}
-         
-         {/* password */}
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            className="input bg-white "
+            placeholder="Email"
+          />
+          {errors.email && (
+            <span className="text-red-500">Email is required</span>
+          )}
+
+          {/* password */}
           <label className="label">Password</label>
           <input
-            {...register("password", { required: true, minLength:6 })}
+            {...register("password", { required: true, minLength: 6 })}
             type="password"
             className="input bg-white"
             placeholder="Password"
           />
-          {errors.password?.type === "minLength" && <span className="text-red-500">Password must be at least 6 characters</span>}
-         
+          {errors.password?.type === "minLength" && (
+            <span className="text-red-500">
+              Password must be at least 6 characters
+            </span>
+          )}
         </fieldset>
-          <button className="bg-color1 px-4 py-2 mt-4 font-semibold w-80">Register</button>
+        <button className="bg-color1 px-4 py-2 mt-4 font-semibold w-80">
+          Register
+        </button>
       </form>
       {/* already have an account? */}
       <p className="mt-4">
-        Already have an account? <Link to="/login" className="text-color2">Login</Link>
+        Already have an account?{" "}
+        <Link to="/login" className="text-color2">
+          Login
+        </Link>
       </p>
     </div>
   );
